8814e462d3a911331f5b38f4faa53b116d25d0ac
Steffen Vogel <post@steffenvogel.de>
2019-04-25 22:12:23 +0200

    remove copyright and license info from python files

--- transWhat/whatsappbackend.py
+++ transWhat/whatsappbackend.py
@@ -1,26 +1,3 @@
-__author__ = "Steffen Vogel"
-__copyright__ = "Copyright 2015-2017, Steffen Vogel"
-__license__ = "GPLv3"
-__maintainer__ = "Steffen Vogel"
-__email__ = "post@steffenvogel.de"
-
-"""
- This file is part of transWhat
-
- transWhat is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- any later version.
-
- transwhat is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with transWhat. If not, see <http://www.gnu.org/licenses/>.
-"""
-
 from Spectrum2.backend import SpectrumBackend
 from Spectrum2 import protocol_pb2
 
