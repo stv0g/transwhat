5aab009751b1b304c6379899f189bca2bd363c95
Wojciech Paciorek <arkkors@users.noreply.github.com>
2021-06-17 16:39:44 +0200

    first tests for session

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from spectrum2 import Backend
 from transWhat.session import Session
 from unittest.mock import create_autospec
+from yowsup.stacks import YowStack
 
 
 @pytest.fixture
@@ -14,3 +15,6 @@ def mock_backend() -> Backend:
     yield create_autospec(Backend)
 
 
+@pytest.fixture
+def mock_yowstack() -> YowStack:
+    yield create_autospec(YowStack)
--- tests/test_session.py
+++ tests/test_session.py
@@ -1,6 +1,52 @@
+import pytest
+
 from transWhat.session import Session
+from typing import Optional
+from yowsup.stacks import YowStack
+from yowsup.layers import YowLayerEvent
+from yowsup.layers.network import YowNetworkLayer
 
 
-def test_setup_works(mock_backend):
-    sess = Session(backend=mock_backend, user="mock@user.com", legacyName="", extra=[])    
+@pytest.fixture
+def session_instance(mock_backend, mock_yowstack):
+    mock_user = "mock@user.com"
+    mock_legacy_name = "legacy name"
 
+    sess = Session(
+        backend=mock_backend, user=mock_user, legacyName=mock_legacy_name, extra=[]
+    )
+    sess.stack = mock_yowstack
+
+    yield sess
+
+
+def assert_event_called(sess: Session, event_name):
+
+    # This essentially gets the first argument of a broadcastEvent call
+    # This is dirty, but we can do nothing about it for now
+    events = [
+        call[0][0] if call[0] else call[1].get("name")
+        for call in sess.stack.broadcastEvent.call_args_list
+    ]
+
+    assert event_name in [event.name for event in events]
+
+
+def get_events(sess: Session):
+    sess.stack.broadcastEvent.call
+
+
+def test_login_works(session_instance):
+    password = "test password"
+
+    session_instance.login(password)
+    assert session_instance.password == password
+    assert_event_called(session_instance, YowNetworkLayer.EVENT_STATE_CONNECT)
+
+
+def test_logout_works(session_instance):
+    session_instance.loggedIn = True
+
+    session_instance.logout()
+    assert not session_instance.loggedIn
+    assert_event_called(session_instance, YowNetworkLayer.EVENT_STATE_DISCONNECT)
